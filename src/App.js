cd0e58c0b0de3bca596a1942868d9218207752c5
Adilson Cruz <adilsonjcruz@live.com>
2021-05-30 22:13:44 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
+import Menu from "./components/Menu";
+import TabelaLivros from "./components/TabelaLivros";
+import CadastrarLivros from "./components/CadastrarLivros";
+import NotFound from "./components/NotFound";
+import SimpleStorage from "react-simple-storage";
+class App extends Component {
+  
+  state = {
+    livros: [
+      // {
+      //   id: 1,
+      //   isbn: "978-85-7522-403-8",
+      //   titulo: "HTML5 - 2ª Edição",
+      //   autor: "Maurício Samy Silva"
+      // },
+      // {
+      //   id: 2,
+      //   isbn: "978-85-7522-807-4",
+      //   titulo: "Introdução ao Pentest",
+      //   autor: "Daniel Moreno"
+      // },
+      // {
+      //   id: 3,
+      //   isbn: "978-85-7522-780-8",
+      //   titulo: "Internet das Coisas para Desenvolvedores",
+      //   autor: "Ricardo da Silva Ogliari"
+      // }      
+    ]
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  inserirLivros = livro => {
+    livro.id = this.state.livros.length + 1;
+    this.setState({
+      livros: [...this.state.livros, livro]
+    });
+  };
+
+  editarLivro = livro => {
+    const index = this.state.livros.findIndex(p => p.id === livro.id);
+    const livros = this.state.livros
+      .slice(0, index)
+      .concat(this.state.livros.slice(index + 1));
+    const newLivros = [...livros, livro].sort((a, b) => a.id = b.id);
+    this.setState({
+      livros: newLivros
+    })
+  };
+
+  removerLivro = livro => {
+    if(window.confirm("Remover este livro?")) {
+      const livros = this.state.livros.filter(p => p.isbn !== livro.isbn);
+      this.setState({ livros });
+    }
+  };
+    
+  render(){
+    return(
+      <Router>
+        <SimpleStorage parent={this} />
+        <Menu />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <TabelaLivros 
+                             livros={this.state.livros} 
+                             removerLivro={this.removerLivro}
+                          /> }
+          />
+          <Route 
+            exact
+            path="/cadastrar"
+            render={() => (
+              <CadastrarLivros
+                inserirLivro = { this.inserirLivros }
+                livro = {{ id: 0, isbn: "", titulo: "", autor: "" }} 
+              />
+            )}
+          />
+          <Route 
+            exact
+            path="/editar/:isbnLivro"
+            render={props => {
+              const livro = this.state.livros.find(
+                livro => livro.isbn === props.match.params.isbnLivro
+              );
+              if (livro) {
+                return(
+                  <CadastrarLivros
+                    editarLivro={this.editarLivro}
+                    livro={livro}
+                  />
+                );
+              } else {
+                return <Redirect to="/" />;
+              }
+            }}
+          />
+          <Route component={NotFound} />          
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
